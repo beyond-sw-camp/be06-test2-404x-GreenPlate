a505bdc8a8a2aa8f7f099f483d229970784d0abd
mangoade100g <somethingisnothing125@gmail.com>
2024-06-20 14:20:17 +0900

    [Init] 프로젝트 생성

---
+++ src/main/java/com/example/Spring404x/Spring404xApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring404x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring404xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring404xApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring404x/Spring404xApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring404x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring404xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
