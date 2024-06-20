3f52c218d62954fbe020b97216424fedbfeb210d
dyun23 <ozsally@naver.com>
2024-06-20 16:31:21 +0900

    [Feat] 유저 회원가입 기능 구현(#4)

---
+++ src/main/java/com/example/Spring404x/user/UserController.java
@@ -0,0 +1,25 @@
+package com.example.Spring404x.user;
+
+import com.example.Spring404x.user.model.UserSignUpReq;
+import com.example.Spring404x.user.model.UserSignUpRes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/signup")
+    public ResponseEntity<UserSignUpRes> signup(@RequestBody UserSignUpReq dto) {
+        UserSignUpRes res = userService.signUp(dto);
+        return ResponseEntity.ok(res);
+    }
+}
